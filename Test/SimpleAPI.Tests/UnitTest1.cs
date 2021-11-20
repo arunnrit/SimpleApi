d6a6582d6f11e7acf740a45a328f6e6d8f3520c0
unknown <ritubansal123@gmail.com>
2021-11-20 11:20:35 +0530

    first commit

---
+++ Test/SimpleAPI.Tests/UnitTest1.cs
@@ -0,0 +1,20 @@
+using System;
+using Xunit;
+using SimpleAPI.Controllers;
+
+namespace SimpleAPI.Tests
+{
+    public class UnitTest1
+    {
+        WeatherForecastController obj;
+
+        [Fact]
+        public void GetReturnValue()
+        {
+            obj= new WeatherForecastController();
+           var objGet= obj.Get(1);
+            Assert.Equal("Ritu Garg",objGet.Value);
+        }
+
+    }
+}
